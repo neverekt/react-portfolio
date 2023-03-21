c39a3f8839446feae4ba621a3e8c55ad7cac9ce0
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-03-21 18:15:07 +0000

    small edits

--- src/assets/Project.jsx
+++ src/assets/Project.jsx
@@ -3,25 +3,12 @@ import { useParams } from 'react-router-dom';
 import projectsData from './projectsData.json';
 import { CodeBracketIcon, PaperAirplaneIcon } from '@heroicons/react/20/solid'
 
-const people = [
-  {
-    name: 'Jane Cooper',
-    title: 'Paradigm Representative',
-    role: 'Admin',
-    email: 'janecooper@example.com',
-    telephone: '+1-202-555-0170',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
-  },
-  // More people...
-]
-
 export default function Project() {
     const { id } = useParams();
     const project = projectsData.find(p => p.id === id);
 
   return (
-    <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+    <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <li
           key={project.id}
           className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
