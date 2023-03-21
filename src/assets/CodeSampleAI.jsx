fdac453667c08d15f001c254d14a030a9b0136b8
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-03-21 22:13:20 +0000

    added new components for future and edited the codesampleAI component for more relevent code

--- src/assets/CodeSampleAI.jsx
+++ src/assets/CodeSampleAI.jsx
@@ -4,29 +4,41 @@ import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 export default function CodeSampleAI() {
     return (
-        <SyntaxHighlighter language="json" style={vscDarkPlus}>
-            {`{
-                "id": "customer-interaction-march-2023",
-                "object": "customer-inquiry-0223",
-                "created": 1661516177,
-                "model": "customer-behavior-model-0051",
-                "usage": {
-                    "prompt_tokens": 135,
-                    "completion_tokens": 75,
-                    "total_tokens": 210
-                },
-                "choices": [
-                    {
-                        "message": {
-                            "role": "assistant",
-                            "content": "\\n\\nThis is a test!"
-                        },
-                        "finish_reason": "stop",
-                        "index": 0
+    <SyntaxHighlighter language="json" style={vscDarkPlus}>
+        {`{
+            "iteration": 10,
+            "batch": 3,
+            "total_loss": 1.1234,
+            "avg_loss": 0.4567,
+            "inputs": [
+                {
+                    "text": "Annual budget report for Q1.",
+                    "metadata": {
+                        "label": "budget",
+                        "source": "financial data"
                     }
-                ]
-            }`
-            }
+                },
+                {
+                    "text": "Sales performance analysis for the past year.",
+                    "metadata": {
+                        "label": "sales",
+                        "source": "CRM system"
+                    }
+                }
+            ],
+            "model_params": {
+            "hidden_dim": 768,
+            "num_layers": 12,
+            "num_heads": 12,
+            "dropout": 0.1
+        },
+        "training_params": {
+            "batch_size": 32,
+            "learning_rate": 0.001,
+            "num_epochs": 20
+        },
+        "training_time": "02:34:56"
+        }`}
         </SyntaxHighlighter>
     );
 }
