cd798ac08d138bce54c17bf15142b2213e79eb13
Jack Turner <34825609+neverekt@users.noreply.github.com>
2023-03-21 18:37:29 +0000

    edited css to make it even layout

--- src/assets/ProjectCard.jsx
+++ src/assets/ProjectCard.jsx
@@ -1,55 +1,45 @@
 import { CodeBracketIcon, PaperAirplaneIcon } from '@heroicons/react/20/solid'
 
 function ProjectCard({ project }) {
-    return (
+  return (
     <div className="col-md-4 mb-4">
-     <div className="relative isolate bg-gray-900">
-        <li
-          key={project.id}
-          className="col-span-1 flex flex-col divide-y divide-gray-200 rounded-lg bg-white text-center shadow"
-        >
+      <div className="relative isolate bg-gray-900 flex flex-col h-full">
+        <li className="col-span-1 flex flex-col rounded-lg bg-white text-center shadow overflow-hidden flex-1">
           <div className="flex flex-1 flex-col p-8">
-            <img className="mx-auto h-32 w-32 flex-shrink-0 rounded-full" src={project.image} alt={project.title} />
-            <h3 className="mt-6 text-sm font-medium text-gray-900">{project.name}</h3>
-            <dl className="mt-1 flex flex-grow flex-col justify-between">
-              <dt className="sr-only">Title</dt>
-              <dd className="text-sm text-gray-500">{project.title}</dd>
-              <dt className="sr-only">Description</dt>
-              <dd className="mt-3">
-                <span className="rounded-full px-2 py-1 text-xs font-medium">
-                  {project.description}
-                </span>
-              </dd>
-            </dl>
+            <img className="mx-auto h-32 w-32 rounded-full object-cover" src={project.image} alt={project.title} />
+            <h3 className="mt-6 text-lg font-medium text-gray-900">{project.name}</h3>
+            <p className="mt-2 text-gray-500 text-sm">{project.title}</p>
+            <div className="mt-4 flex-1">
+              <p className="text-sm text-gray-600">{project.description}</p>
+            </div>
           </div>
-          <div>
-            <div className="-mt-px flex divide-x divide-gray-200">
-              <div className="flex w-0 flex-1">
-                <a
-                  href={project.githubUrl}
-                  className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
-                >
-                  <CodeBracketIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                  View Code
-                </a>
-              </div>
-              <div className="-ml-px flex w-0 flex-1">
-                <a
-                  href={project.deployedUrl}
-                  className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900"
-                >
-                  <PaperAirplaneIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                  View Deployment
-                </a>
-              </div>
+          <div className="bg-gray-100 px-4 py-3">
+            <div className="flex items-center justify-between">
+              <a
+                href={project.githubUrl}
+                className="flex items-center text-gray-800 hover:text-gray-900 transition duration-150 ease-in-out"
+              >
+                <CodeBracketIcon className="h-5 w-5 mr-2" aria-hidden="true" />
+                <span>View Code</span>
+              </a>
+              <a
+                href={project.deployedUrl}
+                className="flex items-center text-gray-800 hover:text-gray-900 transition duration-150 ease-in-out"
+              >
+                <PaperAirplaneIcon className="h-5 w-5 mr-2" aria-hidden="true" />
+                <span>View Deployment</span>
+              </a>
             </div>
           </div>
         </li>
-        </div>
+      </div>
     </div>
-    );
+  );
 }
+
 export default ProjectCard;
+
+
 
 // const { id } = useParams();
 // const project = projectsData.find(p => p.id === id);
